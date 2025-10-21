50442a04d083ceba21d3b673398ead40a09e9083
jesusrodriguez-07 <jesus.rodriguez@iesnervion.es>
2025-10-21 13:44:28 +0200

    j

--- src/conditionals/Ejercicio01.java
+++ src/conditionals/Ejercicio01.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
-		//Definimos la variable para el número introducido
+		// Definimos la variable para el número introducido
 		int num;
+		// Definimos la variable para saber si es capicua o no
+		boolean esCapicua;
 		// Creamos el scanner
 		Scanner sc = new Scanner(System.in);
-		//Pedimos al usuario que introduzca el número comprendido en los valores del enunciado
+		// Pedimos al usuario que introduzca el número comprendido en los valores del
+		// enunciado
 		System.out.println("Introduce un número entre 0 y 9999: ");
 		num = sc.nextInt();
 		// hacemos que si no esta entre esos valores se acabe la ejecución del programa
 		if (num < 0 || num > 9999) {
-			System.out.println("Error, el número introducido es inválido");
+			System.out.println("Error, el número introducido es inválido"); // Ahora sacamos los valores de cada
+																			// posicion del número
 		} else {
+			// Guardamos en cada variable el valor de el dígito de cada posición
 			int unidades = num % 10;
 			int decenas = (num / 10) % 10;
 			int centenas = (num / 100) % 10;
 			int miles = (num / 1000) % 10;
 
-			boolean esCapicua;
-
+			// Comparamos dependiendo de si el número es menor de 10 y asi sucesivamente
+			// comparando
+			// los valores necesarios en cada caso
 			if (num < 10) {
 				esCapicua = true;
 			} else if (num < 100) {
@@ -30,9 +36,12 @@ public class Ejercicio01 {
 			} else if (num < 1000) {
 				esCapicua = (unidades == centenas);
 			} else {
+				// En este tenemos que comparar ambos números no solo el primero y el último
 				esCapicua = (unidades == miles) && (decenas == centenas);
 			}
 
+			// Con un if hacemos que si la variable esCapicua es true pues imprima que si es
+			// capicúa
 			if (esCapicua) {
 				System.out.println("El número " + num + " es capicúa.");
 			} else {
--- src/conditionals/Ejercicio02.java
+++ src/conditionals/Ejercicio02.java
@@ -1,9 +1,31 @@
 package conditionals;
 
+import java.util.Scanner;
+
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declaramos la variable para el número introducido por teclado
+		int num;
+		// Declaramos una variable para guardar el valor absoluto final que mostraremos
+		// por pantalla
+		int valorA;
+		// Abrimos el scanner
+		Scanner sc = new Scanner(System.in);
+		// Pedimos al usuario que introduzca por pantalla el número del que quiere saber
+		// el valor absoluto
+		System.out.println("Introduce el número del que quieres saber el valor absoluto");
+		num = sc.nextInt();
+
+		// Mediante un operador ternario Hacemos que si el número es mayor que 0 lo
+		// imprimimos normalmente y si en caso contrario el número es negativo le
+		// cambiamos el simbolo para que sea su valor absoluto
+		valorA = (num > 0) ? num : -num;
+		// Lo imprimimos por pantalla el valor absoluto final
+		System.out.println("El valor absoluto de " + num + " es " + valorA);
+
+		// Cerramos el scanner
+		sc.close();
 
 	}
 
